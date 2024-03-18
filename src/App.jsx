7ac77bc30fcce1b718a4c6a9f44eb409353e8d1b
Melissa L <mel.layn2006@gmail.com>
2024-03-18 15:24:40 -0300

    quarto commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ export default function App() {
           <img src={iconSedans} alt="Ícone dos carros sedan" />
           <h2 className="font-shoulders">Sedans</h2>
           <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+          <a   className="font-lexend" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-green">
           <img src={iconSuvs} alt="icone dos carros SUV" />
           <h2 className="font-shoulders">SUVs</h2>
           <p className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
+          <a  className="font-lexend" href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-dark-green">
           <img src={iconLuxury} alt="icone dos carros de Luxo" />
           <h2 className="font-shoulders" >Luxuosos</h2>
           <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a href="https://www.webmotors.com.br/carros-de-luxo/carros/estoque" target="_blank">Ver mais</a>
+          <a className="font-lexend" href="https://www.webmotors.com.br/carros-de-luxo/carros/estoque" target="_blank">Ver mais</a>
 
         </div>
       </section>
